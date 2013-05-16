ce91d15b3f4efd1af6829bf4e682eac345391b7e
Brian Yee <yee.brianj@gmail.com>
2013-05-16 00:11:33 -0400

    test first view

--- sportsbook/urls.py
+++ sportsbook/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'sportsbook.views.home', name='home'),
+    url(r'^$', 'sportsbook.views.home', name='home'),
     # url(r'^sportsbook/', include('sportsbook.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
